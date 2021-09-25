a76496cc9b26f415cae50e57a0898d07615c198a
Praveen Yadav <praveen.yadav@sanklan.com>
2021-09-25 21:09:48 +0530

    new commit

--- app/src/main/java/com/jmm/brsap/meditell/viewmodel/AddScheduleViewModel.kt
+++ app/src/main/java/com/jmm/brsap/meditell/viewmodel/AddScheduleViewModel.kt
@@ -3,10 +3,14 @@ package com.jmm.brsap.meditell.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.jmm.brsap.meditell.model.Schedule
+import com.jmm.brsap.meditell.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class AddScheduleViewModel :ViewModel(){
+class AddScheduleViewModel @Inject constructor(
+    val authRepository: AuthRepository
+) :ViewModel(){
 
     val selectedDates = MutableLiveData<List<Schedule>>()
 }
