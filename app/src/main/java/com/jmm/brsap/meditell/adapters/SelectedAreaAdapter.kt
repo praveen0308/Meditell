5915cb6aa9f66371dbfca6118c49b1f411a2e855
Praveen Yadav <praveen.yadav@sanklan.com>
2021-10-04 20:09:47 +0530

    new ui started schedule ui done

---
+++ app/src/main/java/com/jmm/brsap/meditell/adapters/SelectedAreaAdapter.kt
@@ -0,0 +1,66 @@
+package com.jmm.brsap.meditell.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.jmm.brsap.meditell.R
+import com.jmm.brsap.meditell.databinding.TemplateAreaListItemBinding
+import com.jmm.brsap.meditell.model.*
+
+class SelectedAreaAdapter() :
+    RecyclerView.Adapter<SelectedAreaAdapter.SelectedAreaViewHolder>() {
+
+
+    private val mList = mutableListOf<Area>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SelectedAreaViewHolder {
+        return SelectedAreaViewHolder(
+            TemplateAreaListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: SelectedAreaViewHolder, position: Int) {
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    fun setAreaList(mList: List<Area>) {
+        this.mList.clear()
+        this.mList.addAll(mList)
+        notifyDataSetChanged()
+    }
+
+    inner class SelectedAreaViewHolder(
+        val binding: TemplateAreaListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener {
+
+            }
+
+
+        }
+
+        fun bind(item: Area) {
+            binding.apply {
+                tvTitle.text = item.name
+                divider70.isVisible = !item.isLast
+            }
+        }
+    }
+
+    interface SelectedAreaInterface {
+
+    }
+
+
+}
